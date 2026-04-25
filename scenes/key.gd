2a972f15f912c0e140633614cc5a5b18640fab8a
Benedikt Baumgarten <baumgarten.benedikt@icloud.com>
2026-04-26 00:59:39 +0200

    Added key-warping to need a sample of only every 3rd key. Also added all sounds for a normal piano

--- scenes/key.gd
+++ scenes/key.gd
@@ -19,12 +19,57 @@ var key_map : Array = ["C", "C#",
 @export var octave : int
 @export var keyID : int
 
+#These should be more dynamically more centrally handled, 
+#depended how pitch of keys are distributed in a octave.
+#e.g. EDO31 instead of EDO12
+var pitchup = 1.0594631
+var pitchdown = 0.94387430765
+
+#What is the distance between the sounds we have
+#For now measured in 1/12 of octave
+var keymodulo = 3
+var keysinoctave = 12
 
 
 func _ready() -> void:
-	var noteString = str(key_map[keyID]) + str(octave)
-	audio_player.stream = load("res://audio/" + noteString + " Long V3.mp3")
+	load_sound()
+
+func warp_from_next(octave : int, keyID : int):
+	#Returns warp_dict : Dictionary = {"warp-origin-octave" : 0, "warp-origin-node" : 0, "warp-factor" : 0.0}
+	var mod = keyID % keymodulo
+	var warp
 	
+	#Pitch up or down depending on whats closer.
+	if mod <= keymodulo / 2.0:
+		warp = mod
+	else:
+		warp = mod - keymodulo
+	var unbound_origin_keyID = keyID - warp
+	
+	
+	#Fix octave 
+	var origin_octave = octave + floor(unbound_origin_keyID / keysinoctave)
+	#Fix origin
+	var origin_keyID = abs(unbound_origin_keyID % keysinoctave)
+	
+	#Get pitch warp-factor
+	var warp_factor : float = 1.0
+	if warp > 0:
+		warp_factor = pitchup
+	elif warp < 0:
+		warp_factor = pitchdown
+	for i in range(abs(warp) - 1):
+		warp_factor *= warp_factor
+	return {"warp-origin-octave" : origin_octave, "warp-origin-keyID" : origin_keyID, "warp-factor" : warp_factor}
+
+func load_sound():
+	#Find out from which sound sample we warp to this node.
+	var warp_dict = warp_from_next(octave, keyID)
+	
+	var noteString = str(key_map[warp_dict.get("warp-origin-keyID")]) + str(warp_dict.get("warp-origin-octave"))
+	audio_player.stream = load("res://audio/" + noteString + " Long V3.mp3")
+	audio_player.pitch_scale = warp_dict.get("warp-factor")
+
 
 func _unhandled_key_input(event: InputEvent) -> void:
 	if(event.is_action_pressed(key)):
